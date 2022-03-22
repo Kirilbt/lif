474d6b3573ee791de96ddf6d805bab7ed0c2b913
Kiril Tucker <kiril.tucker@gmail.com>
2022-03-22 13:32:42 +0100

    seed file prepared

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Create users
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    encrypted_password: '123456',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    info_host: Faker::Lorem.sentence(word_count: 10),
+    address: "#{Faker::Address.street_address}, #{Faker::Address.zip} #{Faker::Address.city}"
+  )
+  Space.create!(
+    user_id: User.last.id,
+    title: ['Big room', 'Small room', 'Living room', 'Appartement with balcony'].sample,
+    category: ['Small', 'Large'].sample,
+    price_day: Faker::Commerce.price,
+    info_space: Faker::Lorem.sentence(word_count: 15),
+    available: true
+  )
+end
+
+puts 'Users created'
+puts 'Spaces created'
