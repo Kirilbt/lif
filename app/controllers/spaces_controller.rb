7af2915626d393a1de6d3c827bd72055bfeab07e
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-03-22 11:48:58 +0100

    started the route and view for show

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,13 @@
 class SpacesController < ApplicationController
+  before_action :set_space, only: [:show]
+
+  def show
+    @spaces = @user.spaces
+  end
+
+  private
+
+  def set_space
+    @user = User.find(params[:id])
+  end
 end
