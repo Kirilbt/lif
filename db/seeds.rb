94cd76fbf471114973030deb9a0fb1615805a28d
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-03-22 14:20:23 +0100

    creating show view and adapting seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+User.destroy_all
+Space.destroy_all
+
+# Create users
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: '123456',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    info_host: Faker::Lorem.sentence(word_count: 10),
+    address: "#{Faker::Address.street_address}, #{Faker::Address.zip} #{Faker::Address.city}"
+  )
+  Space.create!(
+    user_id: User.last.id,
+    title: ['Big room', 'Small room', 'Living room', 'Appartement with balcony'].sample,
+    category: ['Small', 'Large'].sample,
+    price_day: Faker::Commerce.price,
+    info_space: Faker::Lorem.sentence(word_count: 15),
+    available: true
+  )
+end
