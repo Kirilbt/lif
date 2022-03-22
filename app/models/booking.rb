7bd1c46ca4b5f0fad7e5aff780d4f50d409a70db
Kiril Tucker <kiril.tucker@gmail.com>
2022-03-22 17:19:19 +0100

    Model and controller booking with validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,21 @@
 class Booking < ApplicationRecord
   belongs_to :space
   belongs_to :user
+
+  validates :start_date, :end_date, presence: true
+  validate :start_date_after_today?, :end_date_after_start_date?
+
+  private
+
+  def start_date_after_today?
+    if start_date < Date.today
+      errors.add :start_date, "can't be before today"
+    end
+  end
+
+  def end_date_after_start_date?
+    if end_date < start_date
+      errors.add :end_date, "must be after start date"
+    end
+  end
 end
