1fc515994279976636bc8715520162a120386936
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-03-22 15:05:30 +0100

    layout banner

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,17 +1,16 @@
 class SpacesController < ApplicationController
   before_action :set_space, only: [:show]
-  
+
   def index
     @spaces = Space.all
   end
-  
+
   def show
-    @spaces = @user.spaces
   end
 
   private
 
   def set_space
-    @user = User.find(params[:id])
+    @space = Space.find(params[:id])
   end
 end
