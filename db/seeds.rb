690f1da4725e686a70132a2eec977e921c07d172
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-03-24 14:04:25 +0100

    mapbox inside index

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,20 @@ Booking.destroy_all
 Space.destroy_all
 User.destroy_all
 
+adresses = [
+  "Quai d'Ouchy 1, 1006 Lausanne",
+  "Chemin du Laviau 9, 1025 Saint-Sulpice",
+  "Chemin de la Balle 1, 1134 Vufflens-le-Château",
+  "Place de la Cathédrale, 1005 Lausanne",
+  "Avenue Warnery 8, 1110 Morges",
+  "Route de Duillier 50, 1260 Nyon 1",
+  "Chemin du Tolovaux 12, 1070 Puidoux",
+  "Rue Michel-Servet 1, 1206 Genève",
+  "Chemin du Petit-Bel-Air 2, 1226 Thônex",
+  "Chemin des Grands Vignes 9, 1275 Chéserex"
+]
+
+counter = 0
 
 # Create users
 10.times do
@@ -19,7 +33,8 @@ User.destroy_all
     category: ['Small', 'Large'].sample,
     price_day: Faker::Commerce.price,
     info_space: Faker::Lorem.sentence(word_count: 15),
-    address: "#{Faker::Address.street_address}, #{Faker::Address.zip} #{Faker::Address.city}",
+    address: adresses[counter],
     available: true
   )
+  counter += 1
 end
