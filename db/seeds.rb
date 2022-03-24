951641d9803262c02c046689fa8a9ad4eb2c32c1
Patrick Almeida <patrick.c.almeida@gmail.com>
2022-03-24 11:37:46 +0100

    seed a little better - sign in ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,14 @@ User.destroy_all
     password: '123456',
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    info_host: Faker::Lorem.sentence(word_count: 10)
+    info_host: Faker::Quote.matz
   )
   Space.create!(
     user_id: User.last.id,
     title: ['Big room', 'Small room', 'Living room', 'Appartement with balcony'].sample,
     category: ['Small', 'Large'].sample,
     price_day: Faker::Commerce.price,
-    info_space: Faker::Lorem.sentence(word_count: 15),
+    info_space: Faker::Quote.jack_handey,
     address: "#{Faker::Address.street_address}, #{Faker::Address.zip} #{Faker::Address.city}",
     available: true
   )
